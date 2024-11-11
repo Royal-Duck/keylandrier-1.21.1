dfc174b3c79e07a9e0b29c777b88c2c59f71c3ab
saminouche <samueldescours@gmail.com>
2024-11-11 17:53:29 +0100

    initial commit

---
+++ src/main/java/fr/royalduck/keylandrier/KeylandrierDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.royalduck.keylandrier;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class KeylandrierDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
